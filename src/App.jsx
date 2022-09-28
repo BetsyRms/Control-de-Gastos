766cd0c687b8432f476edf9985d6ebc8a1eeda84
Betsy <aby_7710@hotmail.com>
2022-09-28 17:05:27 -0600

    elimnar gastos, grafica, presupuesto ls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
@@ -7,31 +7,68 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [gastos, setGastos] = useState([])
+
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  )
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
-  const [gastos, setGastos] = useState([])
+  
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length>0){
+      setModal(true)      
+      setTimeout(()=>{
+        setAnimarModal(true)
+      }, 200)
+    }
+  }, [gastoEditar])
+
+  useEffect( () => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto])
+
+  useEffect(()=>{
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  }, [])
 
   const handleNuevoGasto = () => {
     setModal(true)
+    setGastoEditar({})
+
     setTimeout(()=>{
       setAnimarModal(true)
     }, 200)
     
   }
   const guardarGasto = gasto => {
-    gasto.id = generarId()
-    gasto.fecha = Date.now()
-    setGastos([...gastos, gasto])
-
+    console.log(gasto)
+    if(gasto.id){
+      const gastosActualizados = gastos.map(gastoState => gastoState.id === gasto.id ? gasto : gastoState)
+      setGastos(gastosActualizados)
+      setGastoEditar({})
+    } else {
+      gasto.id = generarId()
+      gasto.fecha = Date.now()
+      setGastos([...gastos, gasto])
+    }
     setAnimarModal(false)
     setTimeout(() => {
       setModal(false)
     },300)
   }
   
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter( gasto => gasto.id !== id)
+    setGastos(gastosActualizados)
+  }
   return (
     <div className={modal ? 'fijar' : ''}>
       <Header
@@ -46,6 +83,8 @@ function App() {
           <main>
             <ListadoGastos
               gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
             />
           </main>
           <div className='nuevo-gasto'>
@@ -64,6 +103,8 @@ function App() {
         animarModal={animarModal}
         setAnimarModal={setAnimarModal}
         guardarGasto={guardarGasto}
+        gastoEditar={gastoEditar}
+        setGastoEditar={setGastoEditar}
       />}
       
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 import Mensaje from './Mensaje'
 
-const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
+const Modal = ({
+  setModal, 
+  animarModal, 
+  setAnimarModal, 
+  guardarGasto,
+  gastoEditar,
+  setGastoEditar
+}) => {
 
   const [mensaje, setMensaje] = useState('')
   const [nombre, setNombre] = useState('')
   const [cantidad, setCantidad] = useState('')
   const [categoria, setCategoria] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [id, setId] = useState('')
+
+  useEffect( () => {
+    if(Object.keys(gastoEditar).length>0){
+      setNombre(gastoEditar.nombre)
+      setCantidad(gastoEditar.cantidad)
+      setCategoria(gastoEditar.categoria)
+      setId(gastoEditar.id)
+      setFecha(gastoEditar.fecha)
+    }
+  }, [])
 
   const ocultarModal = () => {
     setAnimarModal(false)
+    setGastoEditar({})
     setTimeout(() => {
       setModal(false)
     },300)
@@ -25,7 +45,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
       },3000)
       return
     }
-    guardarGasto({nombre, cantidad, categoria})
+    guardarGasto({nombre, cantidad, categoria, id, fecha})
   }
   return (
     <div className='modal'>
@@ -40,7 +60,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
         onSubmit={handleSubmit}
         className={`formulario ${animarModal ? "animar" : 'cerrar'}`}
       >
-        <legend>Nuevo gasto</legend>
+        <legend>{gastoEditar.nombre ? 'Editar Gasto' : 'Nuevo Gasto'}</legend>
         {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
         <div className='campo'>
           <label htmlFor="nombre">Nombre gasto</label>
@@ -81,7 +101,8 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
         </div>
         <input 
           type="submit"
-          value="Añadir gasto" />
+          value={gastoEditar.nombre ? 'Guardar Cambios' : 'Añadir'} 
+        />
       </form>
     </div>
   )
